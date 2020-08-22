db7944264bec1a5ad2e849074019bf459518a5af
diegosl <dsl_94@hotmail.com>
2020-08-22 19:29:30 -0300

    Se agregan imagenes de zapatillas a directorio assets. Se habilita assets a pubspec.yaml. Se agrego directorios de estructura de proyecto. Se implemento appbar personalizada custom_appbar. Se implemento primera pagina zapato_page.

---
+++ lib/src/pages/zapato_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:shoesapp/src/widgets/custom_widgets.dart';
+
+class ZapatoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: CustomAppBar(texto: 'For you'));
+  }
+}

bd550157b84f678c763162f0900c06801df1d516
diegosl <dsl_94@hotmail.com>
2020-08-25 14:12:34 -0300

    Se implemento widget inferior de monto de zapatilla y boton de agegar a carrito.

--- lib/src/pages/zapato_page.dart
+++ lib/src/pages/zapato_page.dart
@@ -21,7 +21,8 @@ class ZapatoPage extends StatelessWidget {
                       "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.")
             ],
           ),
-        ))
+        )),
+        AgregarCarritoBoton(monto: 180.0)
       ],
     ));
   }
